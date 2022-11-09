a1e1bcc17412d1ee7151ef4b242cd9f5f80c663f
Oscar <bo.walczak@wp.pl>
2022-11-09 22:28:26 +0100

    Działajaca funckja timer

--- script.js
+++ script.js
@@ -5,48 +5,72 @@ const stopClockBtn = document.querySelector('#stop-clock-btn');
 const DEFAULT_TIME = 1;
 
 
-class Clock {
-    constructor() {
-        this.startingMinutes = DEFAULT_TIME;
-        this.time = this.startingMinutes * 60;
-        this.minutes = Math.floor(this.time / 60);
-        this.seconds = this.time % 60;
+// class Clock {
+//     constructor() {
+//         this.startingMinutes = DEFAULT_TIME;
+//         this.time = this.startingMinutes * 60;
+//         this.minutes = Math.floor(this.time / 60);
+//         this.seconds = this.time % 60;
+//     }
+
+//     update() {
+//         if (this.time >= 0) {
+//             this.minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes
+//             this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+
+//             clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
+//             console.log('działa')
+//         } else {
+//             stopInterval();
+//         }
+//         this.time--;
+//     }
+
+//     showTime() {
+//         clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
+//     }
+
+
+// }
+
+let startingMinutes = DEFAULT_TIME;
+let time = startingMinutes * 60;
+
+function update() {
+
+
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    if (time >= 0) {
+        minutes = minutes < 10 ? "0" + minutes : minutes
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        clockEl.innerHTML = `${minutes}:${seconds}`;
+    } else {
+        stopInterval();
     }
-
-    update() {
-
-        if (this.time >= 0) {
-            this.minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes
-            this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
-
-            clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
-        } else {
-            stopInterval();
-        }
-        this.time--;
-    }
-
-    showTime() {
-        clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
-    }
-
-
+    time--;
 }
 
+function showTime() {
+    clockEl.innerHTML = `${minutes}:${seconds}`;
+}
 
-const clock = new Clock();
+let interval;
 
 function startInterval() {
-    if (!startInterval) {
-        setInterval(startInterval(), 1000);
-
+    if (!interval) {
+        interval = setInterval(update, 1000);
     }
 }
 
 function stopInterval() {
-    clearInterval(startInterval(), 1000);
+    clearInterval(interval);
+    interval = null;
 }
 
-startClockBtn.addEventListener('click', startInterval)
+
+startClockBtn.addEventListener('click', startInterval);
 stopClockBtn.addEventListener('click', stopInterval)
 
