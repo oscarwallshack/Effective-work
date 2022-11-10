98f2117c2bacd1bd7ff638ffe28aae78a70b4ecc
Oscar <bo.walczak@wp.pl>
2022-11-10 22:50:22 +0100

    hide & show settings popup

--- script.js
+++ script.js
@@ -2,57 +2,11 @@ const clockEl = document.querySelector('.clock')
 const startClockBtn = document.querySelector('#start-clock-btn')
 const stopClockBtn = document.querySelector('#stop-clock-btn');
 
+const settingsBtn = document.querySelector('#settings-btn')
+const settingsPopup = document.querySelector('.settings')
+const closeBtn = document.querySelector('.close');
+
 const DEFAULT_TIME = 0.2;
-
-
-// class Clock {
-//     constructor() {
-//         this.startingMinutes = DEFAULT_TIME;
-//         this.time = this.startingMinutes * 60;
-//         this.minutes = Math.floor(this.time / 60);
-//         this.seconds = this.time % 60;
-//     }
-
-//     update() {
-//         if (this.time >= 0) {
-//             this.minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes
-//             this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
-
-//             clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
-//             console.log('działa')
-//         } else {
-//             stopInterval();
-//         }
-//         this.time--;
-//     }
-
-//     showTime() {
-//         clockEl.innerHTML = `${this.minutes}:${this.seconds}`;
-//     }
-
-
-// }
-
-// function update() {
-
-
-//     let minutes = Math.floor(time / 60);
-//     let seconds = time % 60;
-
-//     if (time >= 0) {
-//         minutes = minutes < 10 ? "0" + minutes : minutes
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//         clockEl.innerHTML = `${minutes}:${seconds}`;
-//     } else {
-//         stopInterval();
-//     }
-//     time--;
-// }
-
-// function showTime() {
-//     clockEl.innerHTML = `${minutes}:${seconds}`;
-// }
 
 let startingMinutes = DEFAULT_TIME;
 let time = startingMinutes * 60;
@@ -91,6 +45,24 @@ function stopInterval() {
     interval = null;
 }
 
+function hideSettings() {
+    settingsPopup.style.display = 'none'
+}
+
+function showSettings(){
+    settingsPopup.style.display = 'block'
+}
+
+function outsideClick(e) {
+    if(e.target == settingsPopup){
+        hideSettings()
+    }
+}
+
+
+settingsBtn.addEventListener('click', showSettings)
+closeBtn.addEventListener('click', hideSettings)
+window.addEventListener('click', outsideClick)
 
 startClockBtn.addEventListener('click', startInterval);
 stopClockBtn.addEventListener('click', stopInterval)
